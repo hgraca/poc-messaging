18194a4e3cd58f9b4cd7daf0af38d1ab4925cf0e
Herberto Graca <herberto.graca@gmail.com>
2021-12-01 17:29:09 +0100

    Add makefile command to setup the PHP project

---
+++ php/Makefile.defaults.mk
@@ -0,0 +1,5 @@
+# This file contains the default variables used in the Makefile
+# If you want to change them, duplicate the file, name it "Makefile.defaults.custom.mk" and make the changes you want
+
+COMPOSER_VERSION='2.1.14'
+COMPOSER='./bin/composer'
