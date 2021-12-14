0bb9d0acf4d1c03650e8daa650c9b8c41a5a225a
Herberto Graca <herberto.graca@gmail.com>
2021-12-14 14:07:51 +0100

    Optionally automate running the containers commands

--- php/bin/entrypoint.php
+++ php/bin/entrypoint.php
@@ -1,6 +1,28 @@
 <?php
 echo $_ENV['CONTAINER_ROLE'] . "\n";
 
-while(true) {
-    sleep(60); // to make the container stay alive
+function keepAlive(): void
+{
+    while(true) {
+        sleep(60);
+    }
+}
+
+switch ($_ENV['CONTAINER_ROLE']) {
+    case 'producer':
+        if ($_ENV['CONTAINER_ENV'] === 'test') {
+            shell_exec('./bin/console app:messaging:produce');
+        } else {
+            keepAlive();
+        }
+        break;
+    case 'consumer':
+        if ($_ENV['CONTAINER_ENV'] === 'test') {
+            shell_exec('./bin/console messenger:consume async');
+        } else {
+            keepAlive();
+        }
+        break;
+    default:
+        keepAlive();
 }
