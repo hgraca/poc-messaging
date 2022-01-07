00fce313b53fa353bec412f92d8e3da71dbab6d1
Joep Geevers <joepgeevers@gmail.com>
2022-01-07 12:02:24 +0100

    rearrange project structure

---
+++ dotnet/Application/Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PocMessaging.Application.Console
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
